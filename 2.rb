7260017b62845e0f20e48d6d32f37a3d87fbbbb1
psn <psn@shenMacAir.(none)>
2012-07-27 01:27:33 +0800

    commit2

---
+++ 2.rb
@@ -0,0 +1,12 @@
+print "Hello \"Ruby\".\n"
+print 'hello \\ \'ruby2\'\n'
+print "\n"
+print ("hello", "ruby3", "\n")
+puts ("puts ruby")
+p 100
+p "100"
+p "\\\"\n"
+puts "钱申"
+puts 10
+puts "10"
+puts "end\\\'\"\n"
